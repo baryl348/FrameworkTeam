7eab432bc49b54061a76505c2d2888c0d0e05c1b
batyl348 <edotov.egor@gmail.com>
2021-01-28 15:34:31 +0300

    m

---
+++ src/API/api.test.ts
@@ -0,0 +1,23 @@
+/* eslint-disable prettier/prettier */
+// eslint-disable-next-line prettier/prettier
+/* eslint-disable no-unused-vars */
+/* eslint-disable prettier/prettier */
+import axios from "axios";
+// import { taskUrl } from "./api";
+// jest.mock("axios");
+// describe("taskUrl", () => {
+//     it("Success response data", async () => {
+//         const data = {
+//             item: [
+//                 {
+//                     id: '1',
+//                     text: 'a',
+//                 }
+//             ]
+//         }
+//     }
+//           // axios.get.mockImplementationOnce(() => Promise.resolve())
+// })
+
+
+//  Не знаю как протестрировать Api(axios), буду изучать как писать тесты
